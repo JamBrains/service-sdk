b8a8dfd5d0cdf74cf03c94b735bd0aca9171ddc0
Francisco Aguirre <franciscoaguirreperez@gmail.com>
2025-10-05 19:17:50 -0300

    Add bless host function (#8)
    Added the `bless` host function and an example calling it.

--- sdk/host_functions_untyped.c
+++ sdk/host_functions_untyped.c
@@ -9,6 +9,7 @@
 /* READ */ POLKAVM_IMPORT_WITH_INDEX(3, uint64_t /* len_or_u64_max */, _jb_host_read_untyped, uint64_t /* service_id */, uint64_t /*key_ptr*/, uint64_t /*key_len*/, uint64_t /*out_ptr*/, uint64_t /*out_offset*/, uint64_t /*out_len*/);
 /* WRITE */ POLKAVM_IMPORT_WITH_INDEX(4, uint64_t /*len_or_u64_max*/, _jb_host_write_untyped, uint64_t /*key_ptr*/, uint64_t /*key_len*/, uint64_t /*value_ptr*/, uint64_t /*value_len*/);
 /* INFO */ POLKAVM_IMPORT_WITH_INDEX(5, uint64_t, _jb_host_info_untyped, uint64_t /*service_id*/, uint64_t /*out_ptr*/, uint64_t /*offset*/, uint64_t /*length*/);
+/* BLESS */ POLKAVM_IMPORT_WITH_INDEX(14, uint64_t, _jb_host_bless_untyped, uint64_t /*manager*/, uint64_t /*assigners_ptr*/, uint64_t /*delegator*/, uint64_t /*registrar*/, uint64_t /*extra_ptr*/, uint64_t /*extra_count*/);
 /* LOG */ POLKAVM_IMPORT_WITH_INDEX(100, uint64_t, _jb_host_log_untyped, uint64_t /*level*/, uint64_t /*target*/, uint64_t /*target_len*/, uint64_t /*msg*/, uint64_t /*msg_len*/);
 
 
@@ -32,6 +33,10 @@ uint64_t jb_host_write_untyped(uint64_t key_ptr, uint64_t key_len, uint64_t valu
 
 uint64_t jb_host_info_untyped(uint64_t service_id, uint64_t out_ptr, uint64_t offset, uint64_t length) {
     return _jb_host_info_untyped(service_id, (uint64_t)out_ptr, offset, length);
+}
+
+uint64_t jb_host_bless_untyped(uint64_t manager, uint64_t assigners_ptr, uint64_t delegator, uint64_t registrar, uint64_t extra_ptr, uint64_t extra_count) {
+    return _jb_host_bless_untyped(manager, assigners_ptr, delegator, registrar, extra_ptr, extra_count);
 }
 
 uint64_t jb_host_log_untyped(uint64_t level, uint64_t target, uint64_t target_len, uint64_t msg, uint64_t msg_len) {
